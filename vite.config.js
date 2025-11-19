2548e512841ed3213a90cd349716a16bc9d721b5
Muna18madzinga <theophelosmadzinga@gmail.com>
2025-11-19 19:10:23 +0200

    Fix base path for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VERCEL ? '/' : (process.env.NODE_ENV === 'production' ? '/three-d-portfolio-2025/' : '/'),
+  base: '/',
   plugins: [react(), tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1000,
